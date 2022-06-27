739b7fb8f880737f235d90b0edb349665ddbe778
Bryan Chun <86004607+BryanChunkc@users.noreply.github.com>
2022-06-27 17:37:46 +0800

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "resource_group_name" {
+}
+
+variable "resource_group_location" {
+}
+
+variable "virtual_network_name" {
+}
---
+++ virtual_network.tf
@@ -0,0 +1,22 @@
+resource "azurerm_resource_group" "rgp" {
+    name = var.resource_group_name
+    location = var.resource_group_location
+}
+
+resource "azurerm_virtual_network" "vn" {
+    name = var.virtual_network_name
+    location = var.resource_group_location
+    resource_group_name = var.resource_group_name
+    address_space = [ "10.0.0.0/16" ]
+    depends_on = [
+      azurerm_resource_group.rgp
+    ]
+    subnet {
+        name = "subnet1"
+        address_prefix = "10.0.1.0/24"
+    }
+    subnet {
+        name = "subnet2"
+        address_prefix = "10.0.2.0/24"
+    }
+}
